e02dc334e6c9e0e1393c0665c6f5bde5e7cde492
Lại Minh Sáng <Sanglm2207@gmail.com>
2020-09-30 11:41:55 +0700

    first commit

---
+++ src/main/java/com/example/internationalization/config/WebMvcConfig.java
@@ -0,0 +1,51 @@
+package com.example.internationalization.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    //Bean localeResolver sẽ xác định ngôn ngữ hiển thị trên view bằng cách nào,
+    // ví dụ thông qua session, qua cookies
+    @Bean(name = "localeResolver")
+    public LocaleResolver getLocaleResolver()  {
+        CookieLocaleResolver resolver= new CookieLocaleResolver();
+        resolver.setCookieDomain("myAppLocaleCookie");
+        resolver.setDefaultLocale(Locale.ENGLISH);
+        // 60 minutes
+        resolver.setCookieMaxAge(60*60);
+        return resolver;
+    }
+
+    //Bean messageSource sẽ thực hiện khai báo các file properties
+    @Bean(name = "messageSource")
+    public MessageSource getMessageResource()  {
+        ReloadableResourceBundleMessageSource messageResource= new ReloadableResourceBundleMessageSource();
+
+        // Đọc vào file i18n/messages_xxx.properties
+        // Ví dụ: i18n/messages_en.properties
+        messageResource.setBasename("classpath:i18n/messages");
+        messageResource.setDefaultEncoding("UTF-8");
+        return messageResource;
+    }
+
+    //Method addInterceptors sẽ xác định thay đổi ngôn ngữ hiển thị thông qua param name nào trên trình duyệt.
+    // Ví dụ trên URL có param: ?language=en thì tiếng anh sẽ được hiển thị
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+        localeInterceptor.setParamName("language");
+        registry.addInterceptor(localeInterceptor).addPathPatterns("/*");
+    }
+
+}
